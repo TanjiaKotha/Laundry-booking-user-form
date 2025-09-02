3bcf959cfce985732f359a93aba6fddf1518c6f8
Tanjia Tasnim Kotha <tkotha83@gmail.com>
2025-09-03 02:42:39 +0600

    Update usePickupSlots.js

--- src/hooks/usePickupSlots.js
+++ src/hooks/usePickupSlots.js
@@ -1,10 +1,16 @@
+// src/hooks/usePickupSlots.js
+
 import { useEffect, useState } from 'react'
 
 export default function usePickupSlots() {
   const [slots, setSlots] = useState([])
 
   useEffect(() => {
-    fetch('https://amalaundry.com.au/wp-json/wp/v2/pickup_slot')
+    // MODIFIED: Added cache-busting timestamp
+    const url = new URL('https://amalaundry.com.au/wp-json/wp/v2/pickup_slot');
+    url.searchParams.append('v', new Date().getTime());
+
+    fetch(url)
       .then(res => res.json())
       .then(data => {
         const activeSlots = data
